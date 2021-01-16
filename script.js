f9f3df269e105ab213c978347efb81b2eb200149
Roman Rangel <romanrangel106549@gmail.com>
2021-01-15 22:41:05 -0800

    added icons to pages, added buttons to jumbotron in homepage

---
+++ script.js
@@ -0,0 +1,15 @@
+// var nav = document.getElementById('navbar-expand-lg');
+
+// window.onscroll = function () {
+
+//   if(window.pageYOffset > 100){
+
+//    nav.style.position = "fixed";
+//    nav.style.top = 0;
+
+//    }else{
+//      // nav.style.position = "absolute";
+//      nav.style.position = 'relative'; //fixed
+//      nav.style.top = 100;
+//    }
+// }
